a674a18408d821fcb3764f2e07936f91398486c1
Brandon P <brandonpegula@gmail.com>
2018-04-03 15:54:44 -0400

    Propper Dasbboard page added

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Dashboard = () => {
+	return (
+		<div style={{ textAlign: "center" }}>
+			<h1>Weather Dashboard</h1>
+			Figure out the weather and stuff!
+		</div>
+	);
+};
+
+export default Dashboard;
